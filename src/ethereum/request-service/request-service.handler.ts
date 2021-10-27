fba866ff439828b33b359e99e75202a615248ba9
rubenkristian <rubenkristian96@gmail.com>
2021-10-28 01:45:01 +0700

    catch err to prevent nodejs not terminated

--- src/ethereum/request-service/request-service.handler.ts
+++ src/ethereum/request-service/request-service.handler.ts
@@ -76,54 +76,54 @@ export class RequestServiceService implements OnModuleInit {
       if (savedLastBlock > minimalStartingBlock) {
         lastBlockNumber = savedLastBlock;
       }
+      const endBlock = await this.contract.provider.getBlockNumber();
+
+      /**
+       * Process logs in chunks of blocks
+       * */
+      const chunkSize = 10000;
+      let chunkStart = lastBlockNumber;
+      let chunkEnd = endBlock;
+      // If chunkEnd is more than chunkSize, set chunkEnd to chunkSize
+      if (chunkEnd - chunkStart > chunkSize) {
+        chunkEnd = chunkStart + chunkSize;
+      }
+      while (chunkStart < chunkEnd) {
+        this.logger.log(`Syncing block ${chunkStart} - ${chunkEnd}`);
+
+        const serviceRequestCreatedFilter =
+          this.contract.filters.ServiceRequestCreated(null);
+        const serviceRequestCreatedEvents = await this.contract.queryFilter(
+          serviceRequestCreatedFilter,
+          chunkStart,
+          chunkEnd,
+        );
+        for (const event of serviceRequestCreatedEvents) {
+          const { args, blockNumber, blockHash, transactionHash } = event;
+          await this.handleEvent('ServiceRequestCreated', args[0], {
+            blockNumber,
+            blockHash,
+            transactionHash,
+          });
+        }
+        // TODO:
+        // Handle Other Service Request Events
+
+        // Remember the last block number processed
+        await this.commandBus.execute(
+          new SetLastRequestServiceBlockCommand(chunkEnd),
+        );
+
+        // set chunkStart to 1 block after chunkEnd
+        chunkStart = chunkEnd + 1;
+        // if chunkEnd + chunkSize is more than endBlock,
+        // set chunkEnd to endBlock
+        // else set chunkEnd to (chunkEnd + chunkSize)
+        chunkEnd =
+          chunkEnd + chunkSize > endBlock ? endBlock : chunkEnd + chunkSize;
+      }
     } catch (err) {
       this.logger.log(err);
-    }
-    const endBlock = await this.contract.provider.getBlockNumber();
-
-    /**
-     * Process logs in chunks of blocks
-     * */
-    const chunkSize = 10000;
-    let chunkStart = lastBlockNumber;
-    let chunkEnd = endBlock;
-    // If chunkEnd is more than chunkSize, set chunkEnd to chunkSize
-    if (chunkEnd - chunkStart > chunkSize) {
-      chunkEnd = chunkStart + chunkSize;
-    }
-    while (chunkStart < chunkEnd) {
-      this.logger.log(`Syncing block ${chunkStart} - ${chunkEnd}`);
-
-      const serviceRequestCreatedFilter =
-        this.contract.filters.ServiceRequestCreated(null);
-      const serviceRequestCreatedEvents = await this.contract.queryFilter(
-        serviceRequestCreatedFilter,
-        chunkStart,
-        chunkEnd,
-      );
-      for (const event of serviceRequestCreatedEvents) {
-        const { args, blockNumber, blockHash, transactionHash } = event;
-        await this.handleEvent('ServiceRequestCreated', args[0], {
-          blockNumber,
-          blockHash,
-          transactionHash,
-        });
-      }
-      // TODO:
-      // Handle Other Service Request Events
-
-      // Remember the last block number processed
-      await this.commandBus.execute(
-        new SetLastRequestServiceBlockCommand(chunkEnd),
-      );
-
-      // set chunkStart to 1 block after chunkEnd
-      chunkStart = chunkEnd + 1;
-      // if chunkEnd + chunkSize is more than endBlock,
-      // set chunkEnd to endBlock
-      // else set chunkEnd to (chunkEnd + chunkSize)
-      chunkEnd =
-        chunkEnd + chunkSize > endBlock ? endBlock : chunkEnd + chunkSize;
     }
   }
 }
