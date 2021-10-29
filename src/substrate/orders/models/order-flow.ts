9eb8b2f52018901e8c1c49056f1357147cbbe037
rubenkristian <rubenkristian96@gmail.com>
2021-10-29 21:18:54 +0700

    remove order-flow and service-flow and create service-flow in substrate models

--- src/substrate/orders/models/order-flow.ts
+++
@@ -1,4 +0,0 @@
-export enum OrderFlow {
-    RequestTest = "Request Test", // not sure, change after types.json update
-    StakingRequestService = "Staking Request Service" // not sure, change after types.json update
-}
--- src/substrate/services/models/service-flow.ts
+++
@@ -1,4 +0,0 @@
-export enum ServiceFlow {
-    RequestTest = "Request Test", // not sure, change after types.json update
-    StakingRequestService = "Staking Request Service" // not sure, change after types.json update
-}
