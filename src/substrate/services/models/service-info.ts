5cd5d627c2ef44f7d2bc6cf092ac2119f48f65a3
rubenkristian <rubenkristian96@gmail.com>
2021-11-03 19:25:09 +0700

    update service info

--- src/substrate/services/models/service-info.ts
+++ src/substrate/services/models/service-info.ts
@@ -2,12 +2,12 @@ import { PriceByCurrency } from './price-by-currency';
 
 export class ServiceInfo {
   name: string;
-  category: string;
-  description: string;
   prices_by_currency: PriceByCurrency[];
   expected_duration: string;
+  category: string;
+  description: string;
+  dna_collection_process?: string;
   test_result_sample: string;
   long_description?: string;
   image?: string;
-  dna_collection_process?: string;
 }
