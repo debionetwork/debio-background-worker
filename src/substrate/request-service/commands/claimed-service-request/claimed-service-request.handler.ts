0409a0e76445aa4ac0b963f2163cf78e4dd34cc2
rubenkristian <rubenkristian96@gmail.com>
2021-11-08 19:11:34 +0700

    add request status to claimed and add to params

--- src/substrate/request-service/commands/claimed-service-request/claimed-service-request.handler.ts
+++ src/substrate/request-service/commands/claimed-service-request/claimed-service-request.handler.ts
@@ -1,33 +1,34 @@
 import { Injectable } from "@nestjs/common";
 import { CommandHandler, ICommandHandler } from "@nestjs/cqrs";
 import { ElasticsearchService } from "@nestjs/elasticsearch";
+import { RequestStatus } from "../../models/requestStatus";
 import { ClaimedServiceRequestCommand } from "./claimed-service-request.command";
 
 @Injectable()
 @CommandHandler(ClaimedServiceRequestCommand)
 export class ClaimedServiceRequestHandler implements ICommandHandler<ClaimedServiceRequestCommand> {
-    constructor(private readonly elasticSearchService: ElasticsearchService) {}
+  constructor(private readonly elasticSearchService: ElasticsearchService) {}
 
-    async execute(command: ClaimedServiceRequestCommand) {
-        await this.elasticSearchService.update({
-            index: 'create-service-request',
-            id: command.request.request_hash,
-            refresh: 'wait_for',
-            body: {
-                script: {
-                    lang: 'painless',
-                    source: `
-                        ctx._source.request.lab_address.add(params.lab_address);
-                        ctx._source.request.request_status: params.status;
-                        ctx._source.blockMetadata = params.blockMetaData;
-                    `,
-                    params: {
-                      lab_address: command.request.lab_address,
-                      status: "1",
-                      blockMetaData: command.blockMetadata
-                    },
-                }
-            }
-        })
-    }
+  async execute(command: ClaimedServiceRequestCommand) {
+    await this.elasticSearchService.update({
+      index: 'create-service-request',
+      id: command.request.request_hash,
+      refresh: 'wait_for',
+      body: {
+        script: {
+          lang: 'painless',
+          source: `
+            ctx._source.request.lab_address.add(params.lab_address);
+            ctx._source.request.request_status: params.status;
+            ctx._source.blockMetadata = params.blockMetaData;
+          `,
+          params: {
+            lab_address: command.request.lab_address,
+            status: RequestStatus.Claimed,
+            blockMetaData: command.blockMetadata
+          },
+        }
+      }
+    })
+  }
 }
