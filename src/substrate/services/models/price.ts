9fa005bc7e2887719e1ac2b66990a5bd77825fc8
rubenkristian <rubenkristian96@gmail.com>
2021-11-11 22:52:23 +0700

    fix price is set as BigInt, convert to string.

--- src/substrate/services/models/price.ts
+++ src/substrate/services/models/price.ts
@@ -1,10 +1,10 @@
 export class Price {
-  constructor(_component: any, _value: number) {
+  constructor(_component: any, _value: any) {
     const decoder = new TextDecoder();
 
     this.component = decoder.decode(_component);
-    this.value = _value;
+    this.value = _value.toString();
   }
   component: string;
-  value: number;
+  value: string;
 }
