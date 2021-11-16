9411eed9b4a423adbb902e04489e75238b82aebd
rubenkristian <rubenkristian96@gmail.com>
2021-11-16 20:18:47 +0700

    - update susbtrate unit test - update parameter in service request command - set auth

--- src/substrate/service-request/commands/create-service-request/create-service-request.command.ts
+++ src/substrate/service-request/commands/create-service-request/create-service-request.command.ts
@@ -3,8 +3,7 @@ import { RequestModel } from '../../models/request';
 
 export class CreateServiceRequestCommand {
   request: RequestModel;
-  blockMetadata: BlockMetadata;
-  constructor(args: Array<any>, blockMetadata: BlockMetadata) {
+  constructor(args: Array<any>, public readonly blockMetadata: BlockMetadata) {
     this.request = new RequestModel(
       args[1]["hash_"],
       args[1]["requesterAddress"],
@@ -17,6 +16,5 @@ export class CreateServiceRequestCommand {
       args[1]["status"],
       args[1]["unstakedAt"]
     );
-    this.blockMetadata = blockMetadata;
   }
 }
