3af0b3083c9aa59493e66d1b570c2ade1dd66926
rubenkristian <rubenkristian96@gmail.com>
2021-11-23 14:16:58 +0700

    event for unstaked waiting service request

--- src/substrate/service-request/commands/unstakedwaiting-service-request/unstakedwaiting-service-request.command.ts
+++ src/substrate/service-request/commands/unstakedwaiting-service-request/unstakedwaiting-service-request.command.ts
@@ -1,7 +1,23 @@
 import { BlockMetadata } from "../../models/blockMetadata";
+import { RequestModel } from "../../models/request";
 
 export class UnstakedWaitingServiceRequestCommand {
+  request: RequestModel;
   constructor(args: Array<any>, public readonly blockMetaData: BlockMetadata) {
-    
+    const requestData = args[1];
+    this.request = new RequestModel(
+      requestData["hash_"],
+      requestData["requesterAddress"],
+      requestData["labAddress"],
+      requestData["country"],
+      requestData["region"],
+      requestData["city"],
+      requestData["serviceCategory"],
+      requestData["stakingAmount"],
+      requestData["status"],
+      requestData["createdAt"],
+      requestData["updatedAt"],
+      requestData["unstakedAt"]
+    );
   }
 }
