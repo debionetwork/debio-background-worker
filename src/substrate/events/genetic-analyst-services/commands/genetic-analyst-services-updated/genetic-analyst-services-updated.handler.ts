a7ea29bafe648df2a41ac61d1ba733a5f1b7ae55
rubenkristian <rubenkristian96@gmail.com>
2022-02-08 14:58:34 +0700

    genetic analyst services

---
+++ src/substrate/events/genetic-analyst-services/commands/genetic-analyst-services-updated/genetic-analyst-services-updated.handler.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@nestjs/common';
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { ElasticsearchService } from '@nestjs/elasticsearch';
+import { GeneticAnalystServicesUpdatedCommand } from './genetic-analyst-services-updated.command';
+
+@Injectable()
+@CommandHandler(GeneticAnalystServicesUpdatedCommand)
+export class GeneticAnalystServicesUpdatedHandler
+  implements ICommandHandler<GeneticAnalystServicesUpdatedCommand>
+{
+  constructor(private readonly elasticsearchService: ElasticsearchService) {}
+
+  async execute(command: GeneticAnalystServicesUpdatedCommand) {
+    const { geneticAnalystsServicesModel, blockMetaData } = command;
+
+    await this.elasticsearchService.update({
+      index: 'genetic-analysts-services',
+      id: geneticAnalystsServicesModel.id,
+      refresh: 'wait_for',
+      body: {
+        doc: {
+          info: geneticAnalystsServicesModel.info,
+          blockMetaData: blockMetaData,
+        },
+      },
+    });
+
+    let serviceIndexToDelete = -1;
+
+    const resp = await this.elasticsearchService.search({
+      index: 'genetic-analysts',
+      body: {
+        query: {
+          match: { _id: geneticAnalystsServicesModel.owner_id },
+        },
+      },
+    });
+    const { _source } = resp.body.hits.hits[0];
+    serviceIndexToDelete = _source.services.findIndex(
+      (c) => c.id == geneticAnalystsServicesModel.id,
+    );
+
+    await this.elasticsearchService.update({
+      index: 'genetic-analysts',
+      id: geneticAnalystsServicesModel.owner_id,
+      refresh: 'wait_for',
+      body: {
+        script: {
+          lang: 'painless',
+          source: `
+            if (ctx._source.services_ids.contains(params.id)) {
+              ctx._source.services[params.index] = params.qualification;
+            }
+          `,
+          params: {
+            id: geneticAnalystsServicesModel.id,
+            index: serviceIndexToDelete,
+            qualification: geneticAnalystsServicesModel
+          }
+        }
+      }
+    });
+  }
+}
