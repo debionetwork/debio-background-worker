491c0b6959bf581f8f6b6c938854198037d43687
rubenkristian <rubenkristian96@gmail.com>
2022-02-08 15:11:49 +0700

    genetic analyst service model

---
+++ src/substrate/models/pricesByCurrency.ts
@@ -0,0 +1,16 @@
+import { Price } from '../events/services/models/price';
+import { CurrencyType } from './currencyType';
+
+export class PricesByCurrency {
+  constructor(priceByCurrency: any) {
+    this.currency = priceByCurrency.currency;
+    this.total_price = priceByCurrency.totalPrice;
+    this.price_components = priceByCurrency.priceComponents;
+    this.additional_prices = priceByCurrency.additionalPrices;
+  }
+
+  public currency: CurrencyType;
+  public total_price: string;
+  public price_components: Price[];
+  public additional_prices: Price[];
+}
