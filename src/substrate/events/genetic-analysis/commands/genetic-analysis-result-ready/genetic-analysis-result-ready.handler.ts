0b6e2e35f6905ce38ac3d7498f3c897ffc3bafa7
rubenkristian <rubenkristian96@gmail.com>
2022-02-08 15:04:21 +0700

    genetic analysis

---
+++ src/substrate/events/genetic-analysis/commands/genetic-analysis-result-ready/genetic-analysis-result-ready.handler.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { ElasticsearchService } from '@nestjs/elasticsearch';
+import { GeneticAnalysisResultReadyCommand } from './genetic-analysis-result-ready.command';
+
+@Injectable()
+@CommandHandler(GeneticAnalysisResultReadyCommand)
+export class GeneticAnalysisResultReadyHandler
+  implements ICommandHandler<GeneticAnalysisResultReadyCommand>
+{
+  constructor(private readonly elasticsearchService: ElasticsearchService) {}
+
+  async execute(command: GeneticAnalysisResultReadyCommand) {
+    const { geneticAnalysisModel, blockMetaData } = command;
+
+    await this.elasticsearchService.update({
+      index: 'genetic-analysis',
+      id: geneticAnalysisModel.genetic_analyst_id,
+      body: {
+        doc: {
+          genetic_analyst_tracking_id:
+            geneticAnalysisModel.genetic_analyst_tracking_id,
+          genetic_analyst_order_id: geneticAnalysisModel.genetic_analyst_order_id,
+          comment: geneticAnalysisModel.comment,
+          report_link: geneticAnalysisModel.report_link,
+          updated_at: geneticAnalysisModel.updated_at,
+          status: geneticAnalysisModel.status,
+          blockMetaData: blockMetaData,
+        },
+      },
+    });
+  }
+}
