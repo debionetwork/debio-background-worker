6c25e50032cb306e6dce96f1c40c61eef619890a
rubenkristian <rubenkristian96@gmail.com>
2022-05-12 12:20:12 +0700

    update lab command

--- src/substrate/events/labs/commands/lab-stake-successful/lab-stake-successful.handler.ts
+++ src/substrate/events/labs/commands/lab-stake-successful/lab-stake-successful.handler.ts
@@ -16,14 +16,14 @@ export class LabStakeSuccessfulHandler
       index: 'labs',
       refresh: 'wait_for',
       id: lab.accountId,
-      body: {
+      body : {
         doc: {
           stake_amount: lab.stake_amount,
           stake_status: lab.stake_status,
           unstake_at: lab.unstake_at,
           retrieve_unstake_at: lab.retrieve_unstake_at,
           blockMetaData: command.blockMetaData,
-        },
+        }
       },
     });
   }
--- src/substrate/events/labs/commands/lab-unstake-successful/lab-unstake-successful.handler.ts
+++ src/substrate/events/labs/commands/lab-unstake-successful/lab-unstake-successful.handler.ts
@@ -23,7 +23,7 @@ export class LabUnstakeSuccessfulHandler
           unstake_at: lab.unstake_at,
           retrieve_unstake_at: lab.retrieve_unstake_at,
           blockMetaData: command.blockMetaData,
-        },
+        }
       },
     });
   }
