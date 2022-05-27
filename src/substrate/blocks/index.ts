d4b3cd98fd6d0545a612b695ab6e1b932afdee98
Kirisu <56469224+rubenkristian@users.noreply.github.com>
2022-05-27 17:25:21 +0700

    add try catch when initialize indices (#100)

--- src/substrate/blocks/index.ts
+++ src/substrate/blocks/index.ts
@@ -1,14 +1,17 @@
 export * from './commands/set-last-substrate-block/set-last-substrate-block.command';
 export * from './queries/get-last-substrate-block/get-last-substrate-block.query';
 export * from './commands/delete-all-indexes/delete-all-indexes.command';
+export * from './commands/set-last-substrate-block-old/set-last-substrate-block-old.command';
 
 import { SetLastSubstrateBlockHandler } from './commands/set-last-substrate-block/set-last-substrate-block.handler';
 import { GetLastSubstrateBlockHandler } from './queries/get-last-substrate-block/get-last-substrate-block.handler';
 import { DeleteAllIndexesHandler } from './commands/delete-all-indexes/delete-all-indexes.handler';
+import { SetLastSubstrateBlockOldHandler } from './commands/set-last-substrate-block-old/set-last-substrate-block-old.handler';
 
 export const BlockCommandHandlers = [
   SetLastSubstrateBlockHandler,
   DeleteAllIndexesHandler,
+  SetLastSubstrateBlockOldHandler,
 ];
 
 export const BlockQueryHandlers = [GetLastSubstrateBlockHandler];
