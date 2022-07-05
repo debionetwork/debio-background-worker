6d3f56751120a1bb72c6da0da02163810eea1ee2
Agustinus Theodorus <agustinustheoo@gmail.com>
2022-07-06 04:12:47 +0700

    fix: renamed module to indexer

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
-import { SubstrateModule } from './indexer/indexer.module';
+import { IndexerModule } from './indexer/indexer.module';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config();
 
 @Module({
-  imports: [ScheduleModule.forRoot(), SubstrateModule],
+  imports: [ScheduleModule.forRoot(), IndexerModule],
 })
 export class AppModule {}
