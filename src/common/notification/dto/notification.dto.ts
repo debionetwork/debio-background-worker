247e84539e0ea5cbad08643a8fe94b6fddd69cab
Agustinus Theodorus <agustinustheoo@gmail.com>
2022-07-07 11:17:51 +0700

    fix: failing builds

--- src/common/notification/dto/notification.dto.ts
+++ src/common/notification/dto/notification.dto.ts
@@ -1,63 +1,12 @@
-import { ApiProperty } from '@nestjs/swagger';
-
 export class NotificationDto {
-  @ApiProperty({
-    type: String,
-    description: 'role',
-  })
   role: string;
-
-  @ApiProperty({
-    type: String,
-    description: 'entity_type',
-  })
   entity_type: string;
-
-  @ApiProperty({
-    type: String,
-    description: 'entity',
-  })
   entity: string;
-
-  @ApiProperty({
-    type: String,
-    description: 'description',
-  })
   description: string;
-
-  @ApiProperty({
-    type: Boolean,
-    description: 'read',
-  })
   read: boolean;
-
-  @ApiProperty({
-    type: String,
-    description: 'created_at',
-  })
   created_at: Date;
-
-  @ApiProperty({
-    type: String,
-    description: 'updated_at',
-  })
   updated_at: Date;
-
-  @ApiProperty({
-    type: String,
-    description: 'deleted_at',
-  })
-  deleted_at: Date;
-
-  @ApiProperty({
-    type: String,
-    description: 'from',
-  })
+  deleted_at: Date
   from: string;
-
-  @ApiProperty({
-    type: String,
-    description: 'to',
-  })
   to: string;
 }
