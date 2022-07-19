2bd846644c9bc73ea37ce3246a151671694b21eb
Agustinus Theodorus <37565691+agustinustheo@users.noreply.github.com>
2022-07-19 16:02:02 +0700

    fix: failing start builds (#123)
    * update database using env to google secret manager
    
    * fix unit test
    
    * feat: add process env module defaults
    
    * fix: failing dev start
    
    * fix: uncomment handlers
    
    * feat: update lock file
    
    Co-authored-by: rubenkristian <rubenkristian96@gmail.com>

--- src/common/debio-conversion/debio-conversion.module.ts
+++ src/common/debio-conversion/debio-conversion.module.ts
@@ -1,7 +1,9 @@
+import { GCloudSecretManagerModule } from '@debionetwork/nestjs-gcloud-secret-manager';
 import { Module } from '@nestjs/common';
 import { DebioConversionService } from './debio-conversion.service';
 
 @Module({
+  imports: [GCloudSecretManagerModule.withConfig(process.env.PARENT)],
   providers: [DebioConversionService],
   exports: [DebioConversionService],
 })
