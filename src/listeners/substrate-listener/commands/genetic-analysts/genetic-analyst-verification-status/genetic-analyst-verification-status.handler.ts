ef955a3e7b006d9601e8dc914c1127c2c4638cc8
unknown <jackyrahman14@gmail.com>
2022-08-05 08:15:28 +0700

    rebase

--- src/listeners/substrate-listener/commands/genetic-analysts/genetic-analyst-verification-status/genetic-analyst-verification-status.handler.ts
+++ src/listeners/substrate-listener/commands/genetic-analysts/genetic-analyst-verification-status/genetic-analyst-verification-status.handler.ts
@@ -45,16 +45,6 @@ export class GeneticAnalystVerificationStatusHandler
           21,
         );
       let transactionStatus;
-<<<<<<< HEAD
-=======
-
-      if (geneticAnalyst.verificationStatus === 'Unverified') {
-        transactionStatus = 19;
-        entity = 'Account unverified';
-        notificationDescription =
-          'Your account verification has been rejected.';
-      }
->>>>>>> 69b4940 (fix: error logging GA verification status)
       if (geneticAnalyst.verificationStatus === 'Verified') {
         transactionStatus = 20;
         entity = 'Account verified';
