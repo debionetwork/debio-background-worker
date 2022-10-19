1203e7e756e12c772fe465e8890f1c4e0a6f024c
rubenkristian <rubenkristian96@gmail.com>
2022-10-19 16:38:03 +0700

    update service request listener and indexer

---
+++ src/listeners/substrate-listener/commands/service-request/service-request-updated/service-request-updated.command.ts
@@ -0,0 +1,11 @@
+import { RequestStatus, ServiceRequest } from '@debionetwork/polkadot-provider';
+import { BlockMetaData } from '../../../models/block-metadata.event-model';
+
+export class ServiceRequestUpdatedCommand {
+  requestId: string;
+  status: RequestStatus;
+  constructor(args: Array<any>, public readonly blockMetaData: BlockMetaData) {
+    this.requestId = args[1].toString();
+    this.status = args[2].toString();
+  }
+}
---
+++ src/listeners/substrate-listener/commands/service-request/service-request-updated/service-request-updated.handler.ts
@@ -0,0 +1,143 @@
+import { queryServiceRequestById, RequestStatus, ServiceRequest, setOrderPaid } from '@debionetwork/polkadot-provider';
+import { Logger, Injectable } from '@nestjs/common';
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { NotificationDto } from 'src/common/notification/dto/notification.dto';
+import {
+  DateTimeProxy,
+  NotificationService,
+  SubstrateService,
+  TransactionLoggingService,
+} from '../../../../../common';
+import { TransactionLoggingDto } from '../../../../../common/transaction-logging/dto/transaction-logging.dto';
+import { ServiceRequestUpdatedCommand } from './service-request-updated.command';
+
+@Injectable()
+@CommandHandler(ServiceRequestUpdatedCommand)
+export class ServiceRequestUpdatedHandler
+  implements ICommandHandler<ServiceRequestUpdatedCommand>
+{
+  private readonly logger: Logger = new Logger(
+    ServiceRequestUpdatedCommand.name,
+  );
+
+  constructor(
+    private readonly loggingService: TransactionLoggingService,
+    private readonly dateTimeProxy: DateTimeProxy,
+    private readonly substrateService: SubstrateService,
+    private readonly notificationService: NotificationService,
+  ) {}
+
+  async execute(command: ServiceRequestUpdatedCommand) {
+    const {requestId, status, blockMetaData} = command;
+    this.logger.log(
+      `Service Request Waiting For Unstaked With Hash: ${requestId}!`,
+    );
+
+    const serviceRequest = (await queryServiceRequestById(this.substrateService.api, requestId)).normalize();
+
+    switch(status) {
+      case RequestStatus.Claimed:
+        await this.statusClaimed(serviceRequest, blockMetaData.blockNumber);
+      break;
+      case RequestStatus.Processed:
+        await this.statusProcess(serviceRequest);
+      break;
+      case RequestStatus.Unstaked:
+        await this.statusUnstaked(serviceRequest);
+      break;
+      case RequestStatus.WaitingForUnstaked:
+        await this.statusWaitingForUnstaked(serviceRequest);
+      break;
+      case RequestStatus.Finalized:
+      break;
+    }
+  }
+
+  async statusFinalized(serviceRequest: ServiceRequest) {
+    
+  }
+
+  async statusWaitingForUnstaked(serviceRequest: ServiceRequest) {
+    try {
+      const serviceRequestParent =
+        await this.loggingService.getLoggingByOrderId(serviceRequest.hash);
+      const isServiceRequestHasBeenInsert =
+        await this.loggingService.getLoggingByHashAndStatus(
+          serviceRequest.hash,
+          11,
+        );
+      const stakingLogging: TransactionLoggingDto = {
+        address: serviceRequest.requesterAddress,
+        amount: 0,
+        created_at: this.dateTimeProxy.new(),
+        currency: 'DBIO',
+        parent_id: BigInt(serviceRequestParent.id),
+        ref_number: serviceRequest.hash,
+        transaction_status: 11,
+        transaction_type: 2,
+      };
+
+      if (!isServiceRequestHasBeenInsert) {
+        await this.loggingService.create(stakingLogging);
+      }
+    } catch (error) {
+      this.logger.log(error);
+    }
+  }
+
+  async statusUnstaked(serviceRequest: ServiceRequest) {
+    try {
+      const serviceRequestParent =
+        await this.loggingService.getLoggingByOrderId(serviceRequest.hash);
+      const isServiceRequestHasBeenInsert =
+        await this.loggingService.getLoggingByHashAndStatus(
+          serviceRequest.hash,
+          8,
+        );
+      const stakingLogging: TransactionLoggingDto = {
+        address: serviceRequest.requesterAddress,
+        amount: serviceRequest.stakingAmount,
+        created_at: this.dateTimeProxy.new(),
+        currency: 'DBIO',
+        parent_id: BigInt(serviceRequestParent.id),
+        ref_number: serviceRequest.hash,
+        transaction_status: 8,
+        transaction_type: 2,
+      };
+      if (!isServiceRequestHasBeenInsert) {
+        await this.loggingService.create(stakingLogging);
+      }
+    } catch (error) {
+      this.logger.log(error);
+    }
+  }
+
+  async statusProcess(serviceRequest: ServiceRequest) {
+    await setOrderPaid(
+      this.substrateService.api as any,
+      this.substrateService.pair,
+      serviceRequest['orderId'],
+    );
+  }
+
+  async statusClaimed(serviceRequest: ServiceRequest, blockNumber: number) {
+    const currDateTime = this.dateTimeProxy.new();
+
+    const serviceAvailableNotificationInput: NotificationDto = {
+      role: 'Customer',
+      entity_type: 'Request Service Staking',
+      entity: 'Requested Service Available',
+      reference_id: null,
+      description: `Congrats! Your requested service is available now. Click here to see your order details.`,
+      read: false,
+      created_at: currDateTime,
+      updated_at: currDateTime,
+      deleted_at: null,
+      from: 'Debio Network',
+      to: serviceRequest.requesterAddress,
+      block_number: blockNumber.toString(),
+    };
+
+    await this.notificationService.insert(serviceAvailableNotificationInput);
+  }
+}
