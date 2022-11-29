c5fe5a9da5786b80cec0290f5d279c92e4c87890
rubenkristian <rubenkristian96@gmail.com>
2022-11-29 12:32:09 +0700

    fix unit test

--- src/listeners/substrate-listener/commands/service-request/service-request-updated/service-request-updated.command.ts
+++ src/listeners/substrate-listener/commands/service-request/service-request-updated/service-request-updated.command.ts
@@ -2,10 +2,10 @@ import { RequestStatus } from '@debionetwork/polkadot-provider';
 import { BlockMetaData } from '../../../models/block-metadata.event-model';
 
 export class ServiceRequestUpdatedCommand {
-  hash: string;
+  requestId: string;
   status: RequestStatus;
   constructor(args: Array<any>, public readonly blockMetaData: BlockMetaData) {
-    this.hash = args[0].toString();
+    this.requestId = args[0].toString();
     this.status = args[1].toString();
   }
 }
