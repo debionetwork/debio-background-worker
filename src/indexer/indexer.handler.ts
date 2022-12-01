a3cb8b8617dc8a9bced5422fec1b45960d25ce41
Kirisu <56469224+rubenkristian@users.noreply.github.com>
2022-12-01 17:34:35 +0700

    fix: add menstrual subscription index when initialize (#221)
    fix: add menstrual subscription index when initialize

--- src/indexer/indexer.handler.ts
+++ src/indexer/indexer.handler.ts
@@ -379,6 +379,7 @@ export class IndexerHandler
         'last-block-number-substrate',
         'menstrual-calendar',
         'menstrual-cycle-log',
+        'menstrual-subscription',
       ];
 
       for (const i of indices) {
